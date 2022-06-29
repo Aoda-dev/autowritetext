9f1e1843f315e3252635ad0c43271b088ded797e
Tolegen Kulseitov <hinokamiaoda@gmail.com>
2022-06-29 17:52:29 +0600

    added control speed

--- js.js
+++ js.js
@@ -1,39 +1,44 @@
-const wrapper = document.getElementById("container");
-const btn = document.getElementById("change");
+const wrapper = document.getElementById('container')
+const btn = document.getElementById('change')
+const speedInput = document.querySelector('input[name="speed"]')
 
-const text = "My life is going to be good :)";
+const text = 'My life is going to be good :) '
 
+let idx = 0
+let mode = 'dark'
 
-let idx = 0;
-let mode = "dark";
+let speed = 100 / speedInput.value
 
+let interval = setInterval(writeText, speed)
 
-btn.addEventListener("click", () => {
-  changeDay();
-  console.log(mode);
-});
+btn.addEventListener('click', () => {
+	changeDay()
+})
+
+speedInput.addEventListener('change', (e) => {
+	clearInterval(interval)
+	speed = 100 / e.target.value
+	interval = setInterval(writeText, speed)
+})
 
 function changeDay() {
-  if (mode === "light") {
-    mode = "dark";
+	if (mode === 'light') {
+		mode = 'dark'
+		document.body.classList.add('dark')
+		document.body.classList.remove('light')
+		return
+	}
 
-    document.body.classList.add("dark");
-    document.body.classList.remove("light");
-  } else if (mode === "dark") {
-    mode = "light";
-    document.body.classList.add("light");
-    document.body.classList.remove("dark");
-  }
+	if (mode === 'dark') {
+		mode = 'light'
+		document.body.classList.add('light')
+		document.body.classList.remove('dark')
+		return
+	}
 }
 
 function writeText() {
-  wrapper.innerText = text.slice(0, idx);
-
-  idx++;
-
-  if (idx === text.length) {
-    idx = 0;
-  }
+	wrapper.innerText = text.slice(0, idx)
+	idx++
+	idx === text.length ? (idx = 0) : null
 }
-
-setInterval(writeText, 120);
